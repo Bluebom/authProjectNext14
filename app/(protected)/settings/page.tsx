51a106c08a49163583b53a7bb2c033369d79629e
Franklin <franklin@pbsoft.com.br>
2024-03-23 20:31:06 -0300

    feature: auth settings completed

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,13 +1,9 @@
 'use client';
 
-import { logout } from "@/actions/logout";
-import { Button } from "@/components/ui/button";
-import { useCurrentUser } from "@/hooks/use-current-user";
-
 const SettingsPage = () => {
-    const user = useCurrentUser();
     return (
         <div className="bg-white p-10 rounded-xl">
+            hello
         </div>
     );
 }
